b116a9dcb2b45fe472cd63b7ec8b92fc4f6bbac3
HideBa <baba.papa1120.ba@gmail.com>
2021-03-20 11:13:59 +0900

    fix docker file

--- config/index.mjs
+++ config/index.mjs
@@ -3,7 +3,7 @@ import { default as dbConfiguration} from "./mongodb.config.mjs";
 
 const config = dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.SERVER_PORT;
 
 export const serverConfig = {
   port: port
